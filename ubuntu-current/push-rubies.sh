5bae777059f8b9713c8121acf6740a67a4a3135c
tamino-martinius <dev@zaku.eu>
2018-01-26 19:31:38 +0100

    Push rubies for all release

--- ubuntu-current/push-rubies.sh
+++ ubuntu-current/push-rubies.sh
@@ -2,13 +2,23 @@
 
 set -x
 
+source ./releases
 source ../rubies
+
+mkdir -p ../.build
 
 if [ "$#" -eq 1 ]; then
     RUBIES=( "${1}" )
 fi;
 
-for RUBY in "${RUBIES[@]}"; do
-    echo "> Pushing ${IMAGE_TAG}:${RUBY}"
-    docker push ${IMAGE_TAG}:${RUBY}
+if [ "$#" -eq 2 ]; then
+    RELEASES=( "${1}" )
+    RUBIES=( "${2}" )
+fi;
+
+for RELEASE in "${RELEASES[@]}"; do
+    for RUBY in "${RUBIES[@]}"; do
+        echo "> Pushing ${TAG}"
+        docker push ${IMAGE_TAG}:${TAG}
+    done;
 done;
