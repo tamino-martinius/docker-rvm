0689b6b21646124f663a3ccea8fef3840f90275c
tamino-martinius <dev@zaku.eu>
2018-01-26 19:30:16 +0100

    Build rubies for every release

--- ubuntu-current/build-rubies.sh
+++ ubuntu-current/build-rubies.sh
@@ -2,26 +2,34 @@
 
 set -x
 
+source ./releases
 source ../rubies
 
-mkdir -p work
+mkdir -p ../.build
 
 if [ "$#" -eq 1 ]; then
     RUBIES=( "${1}" )
 fi;
 
-for RUBY in "${RUBIES[@]}"; do
-    DOCKERFILE="work/dockerfile-${RUBY}"
-    cat > $DOCKERFILE <<EOF
-FROM ${IMAGE_TAG}:latest
+if [ "$#" -eq 2 ]; then
+    RELEASES=( "${1}" )
+    RUBIES=( "${2}" )
+fi;
 
-RUN bash -c 'source ~/.rvm/scripts/rvm && rvm install ${RUBY}'
-EOF
+for RELEASE in "${RELEASES[@]}"; do
+    for RUBY in "${RUBIES[@]}"; do
+        TAG="${RUBY}-${RELEASE}"
+        DOCKERFILE="../.build/dockerfile-${TAG}"
+        eval "cat > $DOCKERFILE <<EOF
+$(<dockerfile-ruby)
+EOF" 2> /dev/null
 
-    echo "> Making ${IMAGE_TAG}:${RUBY}"
-    docker build --tag="${IMAGE_TAG}:${RUBY}" --file="${DOCKERFILE}" work
+        echo "> Making ${TAG}"
 
-    if [ -f "${DOCKERFILE}" ]; then
-        rm -f $DOCKERFILE
-    fi;
+        # docker build --tag="${TAG}" --file="${DOCKERFILE}" ../.build
+
+        # if [ -f "${DOCKERFILE}" ]; then
+        #     rm -f $DOCKERFILE
+        # fi;
+    done;
 done;
