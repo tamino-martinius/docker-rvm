445bd0d437575023658ffc9634b33f65c18298fb
tamino-martinius <dev@zaku.eu>
2018-01-27 09:41:00 +0100

    Combined build and push scripts to deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,15 @@
 
 # Ubuntu
 ubuntu/build-releases.sh current
+ubuntu/push-releases.sh current
+
 ubuntu/build-releases.sh end-of-life
+ubuntu/push-releases.sh end-of-life
+
 ubuntu/build-rubies.sh current
+ubuntu/push-rubies.sh current
+
 ubuntu/build-rubies.sh end-of-life
+ubuntu/push-rubies.sh end-of-life
+
+ubuntu/push-latest.sh
